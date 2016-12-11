3d95f07c2bb715d4022fd897212eee60a37f6ae0
mmorshed99 <mmorshed@asu.edu>
2016-12-11 10:10:24 -0500

    adding comments

--- majority_element.py
+++ majority_element.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 ####finding majority element in given numbers
-
+####runtime is (n * logn)
 
 num = raw_input("Enter number of elements   ")
 num = int(num)
@@ -15,7 +15,7 @@ for i in range(0, num):
 
 def majority_element(data):
 
-
+#### if not at the last level...need to divide more
    if len(data) > 2:
      data_half_1 = data[: (len(data)//2)]
      data_half_2 = data[len(data)//2 :]
@@ -27,17 +27,17 @@ def majority_element(data):
          return [1, data[0]]
        else:
          if data[0] == data[1]:
-           return [1, data[0]]
+           return [1, data[0]]  ##first element telling whether there is a majority element, second one telling the element
          else:
-           return [0, -1]
+           return [0, -1]   ### putting -1 assuming -1 will not be a real entry
    
-   if feedback1[0] + feedback2[0] == 0 :
+   if feedback1[0] + feedback2[0] == 0 :   ## if both have no majority element
       return [0, -1]
 
-   elif ((feedback1[0] * feedback1[0]) == 1) and (feedback1[1] == feedback2[1]) :
+   elif ((feedback1[0] * feedback1[0]) == 1) and (feedback1[1] == feedback2[1]) :  ##if both have same majority element
       return [1, feedback1[1]]
 
-   elif feedback1[0] == 1 or feedback2[0] == 1:
+   elif feedback1[0] == 1 or feedback2[0] == 1: ## if either one have majority element, need to know if there is majority element after combinations
     if feedback1[0] == 1:
       total = 0
       for i in range(0, len(data_half_2)):
